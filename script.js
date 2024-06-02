e0cfb47dfbc3b55e33cc7a8728c577faab4a76f9
MileneSA <milenesantosalmeida13@gmail.com>
2024-06-02 00:46:28 -0300

    escalonamento de matrizes

--- script.js
+++ script.js
@@ -4,37 +4,64 @@ function inserirLinha(){
     var novaLinha = tabela.insertRow();
     var colunas = tabela.rows[0].cells.length;
 
-    if(colunas < 5)
-    {
-        for (var i = 0; i < colunas; i++) {
-            var novaCelula = novaLinha.insertCell();
-            novaCelula.contentEditable = "true"; 
-        }
-        for (var i = 0; i < tabela.rows.length; i++) {
-            var novaCelula = tabela.rows[i].insertCell();
-            novaCelula.contentEditable = "true";
-        }
+    for (var i = 0; i < colunas; i++) {
+        var novaCelula = novaLinha.insertCell();
+        novaCelula.contentEditable = "true"; 
     }
+    for (var i = 0; i < tabela.rows.length; i++) {
+        var novaCelula = tabela.rows[i].insertCell();
+        novaCelula.contentEditable = "true";
+    }
+
+    const tabelas = document.querySelectorAll('#novasTabelas');
+    const areaDeCriacaoEquacao = document.getElementById("resolucaoEquacoes");
+    // Itera sobre cada tabela e limpa seu conteúdo
+    tabelas.forEach(tabela => {
+        tabela.innerHTML = '';
+        areaDeCriacaoEquacao.innerHTML = '';
+    });
+
+    matriz.style.display = "none";
 }
 
 function removerLinha() {
+    var tabela = document.getElementById('tabela');
     var linhas = tabela.rows.length;
     var colunas = tabela.rows[0].cells.length;
 
-    if (linhas > 2) {
+    // Remover a última linha se houver mais de uma linha
+    if (linhas > 1) {
         tabela.deleteRow(linhas - 1);
     }
 
-    if (colunas > 3) {
+    // Remover a última coluna se houver mais de uma coluna
+    if (colunas > 1) {
+        // Atualiza o número de linhas depois de remover a última linha
+        linhas = tabela.rows.length;
         for (var i = 0; i < linhas; i++) {
             tabela.rows[i].deleteCell(colunas - 1);
         }
     }
+    const tabelas = document.querySelectorAll('#novasTabelas');
+    const areaDeCriacaoEquacao = document.getElementById("resolucaoEquacoes");
+    
+    // Itera sobre cada tabela e limpa seu conteúdo
+    tabelas.forEach(tabela => {
+        tabela.innerHTML = '';
+        areaDeCriacaoEquacao.innerHTML = '';
+
+    });
+
+    matriz.style.display = "none";
+    
 }
 
+
 function calcular(){
+
     var linhas = tabela.getElementsByTagName("tr");
     var matriz = document.getElementById("matriz");
+    matriz.style.display = "table";
     matriz.innerHTML = ""; 
 
     for (var i = 0; i < linhas.length; i++) {
@@ -63,79 +90,182 @@ function calcular(){
             novaCelula.innerText = numero;
         }
     }   
-    
-    var matriz = document.getElementById("matriz");
 
+    let tabelaReferencia;
+    let tabelaReferenciaAnterior;  
+    for (let table = 0; table < linhas.length - 1; table++) {
+        const areaDeCriacaoTabelas = document.getElementById("novasTabelas");
+        let tabela = document.createElement("table");
+        let nomeTabela = "tabela" + table;           
 
-    var line = matriz.getElementsByTagName("tr");
-    var primeiraLinha = line[0]; 
-    var somaTabelas = document.getElementById("somaTabelas");
-    somaTabelas.innerHTML = "";
-    
-    var novaLinha = somaTabelas.insertRow();
-    
-    for (var i = 0; i < primeiraLinha.cells.length; i++) {
-        var novaCelula = novaLinha.insertCell();
-        novaCelula.innerText = primeiraLinha.cells[i].innerText;
-    }
-    
-    for (let i = 1; i <= line.length - 1; i++) {
-        let pivo = parseFloat(matriz.rows[0].cells[0].innerText.match(/-?\d+/)[0]);
-        let multiplicador = parseFloat(matriz.rows[i].cells[0].innerText.match(/-?\d+/)[0]);
-    
-        let linha = somaTabelas.insertRow();
-    
-        for (let j = 0; j < primeiraLinha.cells.length; j++) {
-            let linhaPivo = parseFloat(matriz.rows[0].cells[j].innerText.match(/-?\d+/)[0]);
-            let linhaMultiplicador = parseFloat(matriz.rows[i].cells[j].innerText.match(/-?\d+/)[0]);
-    
-            let resultado = (pivo * linhaMultiplicador) + (-multiplicador * linhaPivo);
-    
-            let celula = linha.insertCell();
-            celula.innerText = resultado;
+        areaDeCriacaoTabelas.appendChild(tabela);
+        tabela.setAttribute("id",nomeTabela);
+
+        if (table === 0)
+        {
+            tabelaReferencia = matriz;
+            tabelaReferenciaAnterior = matriz;
         }
+        else
+        {
+           tabelaReferenciaAnterior = tabelaReferencia;
+        }
+
+        let line = tabelaReferencia.getElementsByTagName("tr");
+
+            for (let linhasCopiadas = 0; linhasCopiadas <= table; linhasCopiadas++) {
+                let newLine = tabela.insertRow();
+                for (let i = 0; i <= line.length; i++) {
+                    var novaCelula = newLine.insertCell();
+                    let texto = line[linhasCopiadas].cells[i].innerText;
+                    novaCelula.innerText = texto;
+                }
+            }
+
+            for (let multiplyLine = table+1; multiplyLine < line.length; multiplyLine++) {
+                let pivo = parseFloat(tabelaReferenciaAnterior.rows[table].cells[table].innerText.match(/-?\d+/)[0]);
+                let pivoElemento = tabelaReferenciaAnterior.rows[table].cells[table]; // Elemento do DOM correspondente ao pivô
+                let multiplicador = parseFloat(tabelaReferenciaAnterior.rows[multiplyLine].cells[table].innerText.match(/-?\d+/)[0]);
+                let multiplicadorElemento = tabelaReferenciaAnterior.rows[multiplyLine].cells[table]; // Elemento do DOM correspondente ao multiplicador
+                
+                // Destaque o pivô e o multiplicador
+                destacarElementos(pivoElemento, multiplicadorElemento);
+                
+
+                let newLine = tabela.insertRow();
+
+                for (let j = 0; j <= line.length; j++) {
+                    let linhaPivo = parseFloat(tabelaReferenciaAnterior.rows[table].cells[j].innerText.match(/-?\d+/)[0]);
+                    let linhaMultiplicador = parseFloat(tabelaReferenciaAnterior.rows[multiplyLine].cells[j].innerText.match(/-?\d+/)[0]);
+            
+                    let resultado = (pivo * linhaMultiplicador) + (-multiplicador * linhaPivo);
+            
+                    let celula = newLine.insertCell();
+                    celula.innerText = resultado;
+                }
+            }
+        tabelaReferencia = document.getElementById(nomeTabela);
     }
+    let line = tabelaReferencia.getElementsByTagName("tr");
+    const areaDeCriacaoEquacao = document.getElementById("resolucaoEquacoes");
+    let titulo = document.createElement("h2");
+    titulo.innerText = "Equações"; // Set the text content separately
+    areaDeCriacaoEquacao.appendChild(titulo); // Append the element to its parent
+    
+    let numeroLetra;
+
+    for (let equacao = line.length - 1; equacao >= 0; equacao--) {
+        var coluna = tabelaReferencia.rows[equacao].cells.length;
 
 
-    // zerar segunda coluna
-    var somaTabelas = document.getElementById("extraTable");
+        let divEquacao = document.createElement("div");
 
-    var matriz = document.getElementById("somaTabelas");
-    somaTabelas.innerHTML = "";
-    var line = matriz.getElementsByTagName("tr");
-    var primeiraLinha = line[0]; 
-    var segundaLinha = line[1]; 
+        let paragrafo = document.createElement("p");
+        let nomeParagrafo = "paragrafo" + equacao;
     
-    var novaLinha = somaTabelas.insertRow();
-    
-    for (var i = 0; i < primeiraLinha.cells.length; i++) {
-        var novaCelula = novaLinha.insertCell();
-        novaCelula.innerText = primeiraLinha.cells[i].innerText;
-    }
+        areaDeCriacaoEquacao.appendChild(divEquacao);
+        divEquacao.appendChild(paragrafo);
 
-    var novaLinha = somaTabelas.insertRow();
+        paragrafo.setAttribute("id", nomeParagrafo);
+        divEquacao.setAttribute("id", "divEquacao");
+
+        let paragraf = document.createElement("p");
+        let nomeParagraf = "paragraf" + equacao;
+        paragrafo.style.color = "var(--butter)";
+        divEquacao.appendChild(paragraf);
+        paragraf.setAttribute("id", nomeParagraf);
+
+        let paragra = document.createElement("p");
+        let nomeParagra = "paragra" + equacao;
     
-    for (var i = 0; i < segundaLinha.cells.length; i++) {
-        var novaCelula = novaLinha.insertCell();
-        novaCelula.innerText = segundaLinha.cells[i].innerText;
-    }
+        divEquacao.appendChild(paragra);
+        paragra.setAttribute("id", nomeParagra);
+    
+        // Solve the equation
+        let divisor = parseFloat(line[equacao].cells[equacao].innerText);
+        // Select the table
+        const table = document.getElementById('tabela');
+        // Select all rows of the table
+        const row = table.getElementsByTagName('tr');
+        // Select cells of the current row
+        const cells = row[equacao].getElementsByTagName('td');
+    
+        // Initialize an array to store letters
+        let letters = [];
+    
+        // Iterate over the cells of the current row
+        for (let i = 0; i < cells.length; i++) {
+            // Extract letters from each cell and add to the array
+            letters.push(extractLetters(cells[i].innerText));
+        }
+    
+        // Prepare the text to be set as the paragraph's content
+        let texto = [];
+        for (let index = 0; index < letters.length; index++) {
+
+            if (index === letters.length - 1) {
+                // Add "=" before the last element
+                texto.push(`= ${line[equacao].cells[index].innerText} ${letters[index]}`);
+            } else {
+                if(line[equacao].cells[index].innerText != 0)
+                {
+                    texto.push(`${line[equacao].cells[index].innerText} ${letters[index]}`);
+                }
+
+            }
+        }
+    
+        // Set the inner HTML of the paragraph to the constructed text
+        document.getElementById(nomeParagrafo).innerHTML = texto.join(" ");
     
     
-    for (let i = 2; i <= line.length; i++) {
-        let pivo = parseFloat(matriz.rows[1].cells[1].innerText.match(/-?\d+/)[0]);
-        let multiplicador = parseFloat(matriz.rows[i].cells[1].innerText.match(/-?\d+/)[0]);
-    
-        let linha = somaTabelas.insertRow();
-    
-        for (let j = 0; j < primeiraLinha.cells.length; j++) {
-            let linhaPivo = parseFloat(matriz.rows[1].cells[j].innerText.match(/-?\d+/)[0]);
-            let linhaMultiplicador = parseFloat(matriz.rows[i].cells[j].innerText.match(/-?\d+/)[0]);
-    
-            let resultado = (pivo * linhaMultiplicador) + (-multiplicador * linhaPivo);
-    
-            let celula = linha.insertCell();
-            celula.innerText = resultado;
-        } 
-    } 
+        let valores = [];
+        for (let value = 0; value < coluna-1; value++) { // Iterate correctly up to coluna - 1
+            if (value != equacao) {
+            let valor = parseFloat(line[equacao].cells[value].innerText);       
+
+                if (valor != 0) {
+                    valores.push(valor);
+                }
+
+            }
+        }
+
+        let ultimo = parseFloat(line[equacao].cells[coluna - 1].innerText);
+
+        let equationResult;
+        let sumValores;
+        let montarConta;
+        if (valores.length > 0 ) {
+            sumValores = valores.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
+            equationResult = (ultimo - (sumValores*numeroLetra)) / divisor;
+            montarConta = `${letters[equacao]} = (${ultimo}) - (${sumValores} * ${numeroLetra}) / ${divisor}`;
+        } else {
+            equationResult = ultimo / divisor;
+            montarConta = `${letters[equacao]} = (${ultimo})/ ${divisor}`;
+        }
+        numeroLetra = equationResult;
+
+        let result = `${letters[equacao]} = ${equationResult}`;
+
+        alert(numeroLetra);
+        document.getElementById(nomeParagraf).innerHTML = montarConta;
+
+        document.getElementById(nomeParagra).innerHTML = result;
+
+    }    
 }
-   
+
+function limparTodasTabelas() {
+    window.location.reload();
+}
+
+function extractLetters(str) {
+    return str.replace(/[^a-zA-Z]/g, '');
+}
+
+function destacarElementos(pivo, multiplicador) {
+    pivo.setAttribute("id", "pivo");
+    multiplicador.setAttribute("id", "circulo");
+}
+
